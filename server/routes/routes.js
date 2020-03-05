9787a072d92273b6f24d04198fc1fe948bdd452e
JillGlass <glass.nz@gmail.com>
2020-03-05 14:05:30 +1300

    api route updated to remove brackets

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,20 +4,22 @@ const request = require('superagent')
 module.exports = router
 
 // server routes, talking to the external data source
-const vineAPI = 'https://{9aea6c07b34a4b45c1d8d88861fab0d6}:{63c91fc3e75bc93e0d419566986c7c08}@paintvine.myshopify.com/admin/api/2020-01/orders.json?since_id=1863560101962'
+const vineAPI = 'https://9aea6c07b34a4b45c1d8d88861fab0d6:63c91fc3e75bc93e0d419566986c7c08@paintvine.myshopify.com/admin/api/2020-01/orders.json?since_id=1863560101962' // working link in postman
 
-router.get('/joke', (req, res) => {
-    console.log('router hit')
-  return request.get(vineAPI)
+router.get('/order', (req, res) => {
+    console.log('router hit') // yes working
+  return request
+  .get(vineAPI)
   .then(response => {
     res.json(response.body)
   })
+  
   .catch(err => {
     res.status(500).send('API ERROR ' + err.message)
   })
 })
 
-
+// {9aea6c07b34a4b45c1d8d88861fab0d6}:{63c91fc3e75bc93e0d419566986c7c08}@
 
 // basic auth: GET https://{username}:{password}@{shop}.myshopify.com/admin/api/2020-01/shop.json
 
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const router = require('./routes/routes')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
-server.use(cors()) // our server is 3000 our client is 8080
+server.use(cors())
 
 server.use('/api/v1', router)
 
